e3d96770978c90c435e8c3abb36ea9aedd122209
Rijan Dahal <reasondahal719@gmail.com>
2026-02-20 00:18:41 +0545

    Add: comment

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -9,7 +9,7 @@ function Navbar() {
 
                 <a href="index.html" className="shrink-0 w-10 h-10 lg:w-[50px] lg:h-[50px]"><Deerhack /></a>
                 <div className="hidden xl:flex xl:w-[400px]" />
-                <div className="nav-items hidden xl:flex items-center gap-6 lg:gap-10 text-[15px] shrink relative left-10 2xl:left-40">
+                <div className="nav-items hidden xl:flex items-center gap-6 lg:gap-10 text-[15px] shrink relative left-10 2xl:left-40"> {/*sm md lg xl 2xl*/}
                     <a className="hover:text-accent transition">Organizers</a>
                     <a className="hover:text-accent transition">Resources</a>
                 </div>
